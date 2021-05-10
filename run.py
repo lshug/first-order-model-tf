33ad539075b66c7b3572f7f4672bd07d024ae5b0
lshug <levan@shugliashvili.com>
2021-05-10 05:58:00 +0400

    Add explicit device placement

--- run.py
+++ run.py
@@ -26,6 +26,7 @@ parser.add_argument("--exactbatch", dest="exact_batch", action="store_true", hel
 parser.add_argument("--profile", action="store_true", help="enable tensorboard profiling")
 parser.add_argument("--visualizer", action="store_true", help="enable visualizer, only relevant for dataset datamode")
 parser = parser.parse_args()
+device = 'gpu:0' if tf.test.is_gpu_available() else 'cpu'
 if parser.profile:
     tf.debugging.set_log_device_placement(True)
 
@@ -37,64 +38,65 @@ with open(config_path) as f:
 frame_shape = config['dataset_params']['frame_shape']
 num_channels = config['model_params']['common_params']['num_channels']
 
-kp_detector, process_kp_driving, generator, _interpreter_obj_list = load_funcs[parser.target](parser.model, prediction_only=parser.datamode=='file') 
-format_appends = {'direct':'', 'savedmodel':'.savedmodel', 'tflite':'.tflite'}
+with tf.device(device):
+    kp_detector, process_kp_driving, generator, _interpreter_obj_list = load_funcs[parser.target](parser.model, prediction_only=parser.datamode=='file') 
+    format_appends = {'direct':'', 'savedmodel':'.savedmodel', 'tflite':'.tflite'}
 
-if parser.mode == 'animate':
-    if parser.datamode == 'file':
-        source_image, frames, fps = load_image_video_pair(parser.source_image, parser.driving_video, frames=parser.frames, frame_shape=frame_shape, num_channels=num_channels)
-        predictions, _ = animate(source_image, frames, generator, kp_detector, process_kp_driving, 
-                            parser.relative, parser.relative, parser.adapt_movement_scale,
-                            batch_size=parser.batch_size, exact_batch=parser.exact_batch, profile=parser.profile)
-        output = parser.output
-        if not parser.dontappend:
-            output = output + format_appends[parser.target] + '.mp4'
-        save_video(output, predictions, fps=fps)
-    else:
-        outdir = './log/' + parser.model
+    if parser.mode == 'animate':
+        if parser.datamode == 'file':
+            source_image, frames, fps = load_image_video_pair(parser.source_image, parser.driving_video, frames=parser.frames, frame_shape=frame_shape, num_channels=num_channels)
+            predictions, _ = animate(source_image, frames, generator, kp_detector, process_kp_driving, 
+                                parser.relative, parser.relative, parser.adapt_movement_scale,
+                                batch_size=parser.batch_size, exact_batch=parser.exact_batch, profile=parser.profile)
+            output = parser.output
+            if not parser.dontappend:
+                output = output + format_appends[parser.target] + '.mp4'
+            save_video(output, predictions, fps=fps)
+        else:
+            outdir = './log/' + parser.model
+            if not parser.dontappend:
+                outdir = outdir + format_appends[parser.target]
+            if not os.path.exists(outdir):
+                os.mkdir(outdir)    
+            dataset = FramesDataset(**config['dataset_params'])
+            dataset = PairedDataset(initial_dataset=dataset, number_of_pairs=config['animate_params']['num_pairs'])
+            visualizer_params = config['visualizer_params'] if parser.visualizer else None
+            for idx, pair in tqdm(enumerate(dataset)):
+                source_image, frames = pair['source_video'][0][None], pair['driving_video']
+                predictions, visualizations = animate(source_image, frames, generator, kp_detector, process_kp_driving, 
+                                        batch_size=1, exact_batch=parser.exact_batch, profile=parser.profile, visualizer_params=visualizer_params, 
+                                        **config['animate_params']['normalization_params'])
+                result_name = f'{idx}_{pair["source_name"]}_{pair["driving_name"]}.png'
+                full_outdir = outdir + '/' + result_name
+                save_frames_png(full_outdir, predictions)
+                if visualizations is not None:
+                    image_name = result_name + config['animate_params']['format']
+                    visualization_filename = outdir + '/' + image_name
+                    save_visualization(visualization_filename, visualizations)
+    elif parser.mode == 'reconstruction':
+        outdir = './log/' + parser.model + '_reconstruction'
         if not parser.dontappend:
             outdir = outdir + format_appends[parser.target]
         if not os.path.exists(outdir):
-            os.mkdir(outdir)    
+            os.mkdir(outdir)
         dataset = FramesDataset(**config['dataset_params'])
-        dataset = PairedDataset(initial_dataset=dataset, number_of_pairs=config['animate_params']['num_pairs'])
         visualizer_params = config['visualizer_params'] if parser.visualizer else None
-        for idx, pair in tqdm(enumerate(dataset)):
-            source_image, frames = pair['source_video'][0][None], pair['driving_video']
-            predictions, visualizations = animate(source_image, frames, generator, kp_detector, process_kp_driving, 
-                                    batch_size=1, exact_batch=parser.exact_batch, profile=parser.profile, visualizer_params=visualizer_params, 
-                                    **config['animate_params']['normalization_params'])
-            result_name = f'{idx}_{pair["source_name"]}_{pair["driving_name"]}.png'
+        loss_list = []
+        for idx, data in tqdm(enumerate(dataset)):
+            if config['reconstruction_params']['num_videos'] is not None:
+                if idx > config['reconstruction_params']['num_videos']:
+                    break
+            predictions, visualizations, loss = reconstruction(data['video'], generator, kp_detector,
+                                    profile=parser.profile, visualizer_params=visualizer_params,
+                                    )
+            result_name = f'{idx}_{data["name"]}.png'
             full_outdir = outdir + '/' + result_name
             save_frames_png(full_outdir, predictions)
-            if visualizations is not None:
-                image_name = result_name + config['animate_params']['format']
+            if len(visualizations) != 0:
+                image_name = result_name + config['reconstruction_params']['format']
                 visualization_filename = outdir + '/' + image_name
                 save_visualization(visualization_filename, visualizations)
-elif parser.mode == 'reconstruction':
-    outdir = './log/' + parser.model + '_reconstruction'
-    if not parser.dontappend:
-        outdir = outdir + format_appends[parser.target]
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    dataset = FramesDataset(**config['dataset_params'])
-    visualizer_params = config['visualizer_params'] if parser.visualizer else None
-    loss_list = []
-    for idx, data in tqdm(enumerate(dataset)):
-        if config['reconstruction_params']['num_videos'] is not None:
-            if idx > config['reconstruction_params']['num_videos']:
-                break
-        predictions, visualizations, loss = reconstruction(data['video'], generator, kp_detector,
-                                profile=parser.profile, visualizer_params=visualizer_params,
-                                )
-        result_name = f'{idx}_{data["name"]}.png'
-        full_outdir = outdir + '/' + result_name
-        save_frames_png(full_outdir, predictions)
-        if len(visualizations) != 0:
-            image_name = result_name + config['reconstruction_params']['format']
-            visualization_filename = outdir + '/' + image_name
-            save_visualization(visualization_filename, visualizations)
-        loss_list.append(loss)
+            loss_list.append(loss)
     print("Reconstruction loss: {}".format(sum(loss_list)/len(loss_list)))
         
 
