f19d4278178ecc6cba49592d5ff6b653b3387bc4
lshug <levan@shugliashvili.com>
2021-06-22 23:44:48 +0400

    Add non-legacy torch checkpoint loader

--- load_torch_checkpoint.py
+++ load_torch_checkpoint.py
@@ -1,15 +1,35 @@
 import pickle
 import sys
 import numpy as np
+import zipfile
 
 mode = 'numpy'
 
 arr_dict = {}
 scount = 0
 
+def _is_zipfile(f):
+    read_bytes = []
+    start = f.tell()
+
+    byte = f.read(1)
+    while byte != "":
+        read_bytes.append(byte)
+        if len(read_bytes) == 4:
+            break
+        byte = f.read(1)
+    f.seek(start)
+    
+    local_header_magic_number = [b'P', b'K', b'\x03', b'\x04']
+    return read_bytes == local_header_magic_number
+
 def rebuild_as_numpy(storage, storage_offset, size, stride, *args, **kwargs):
+    arr = np.reshape(storage.arr, size)
     arr = np.copy(np.lib.stride_tricks.as_strided(storage.arr, size, stride))
-    arr_dict[storage.ind] = arr
+    if storage.preloaded:
+        np.putmask(arr, np.ones(arr.shape, dtype='bool'), np.reshape(storage.arr, arr.shape))
+    else:
+        arr_dict[storage.ind] = arr
     return arr
 
 class FloatStorage:
@@ -18,6 +38,7 @@ class FloatStorage:
         self.arr = np.zeros(size, dtype='float32')
         self.dtype = 'float32'
         self.ind = scount
+        self.preloaded = False
         scount += 1
 
 class LongStorage:
@@ -26,6 +47,7 @@ class LongStorage:
         self.arr = np.zeros(size, dtype='int64')
         self.dtype = 'int64'
         self.ind = scount
+        self.preloaded = False
         scount += 1
 
 class InjectorUnpickler(pickle.Unpickler):
@@ -41,14 +63,38 @@ class InjectorUnpickler(pickle.Unpickler):
             mod = sys.modules[module]
             klass = getattr(mod, name)
             return klass
-        
+
 def numpy_load(path):
+    z = zipfile.ZipFile(path)
+    loaded_storages = {}
+    def load_tensor(data_type, size, key):
+        name = f'archive/data/{key}'
+        dtype = data_type(0).dtype
+        storage = data_type(size)
+        f = z.open(name)
+        buffer = f.read()
+        storage.arr = np.frombuffer(buffer, dtype=storage.dtype)
+        storage.preloaded = True
+        loaded_storages[key] = storage
+    def persistent_load(saved_id):
+        data = saved_id[1:]
+        data_type, key, location, size = data
+        obj = data_type(size)
+        if key not in loaded_storages:
+            load_tensor(data_type, size, key)
+        storage = loaded_storages[key]
+        return storage
+    unpickler = InjectorUnpickler(z.open('archive/data.pkl'))
+    unpickler.persistent_load = persistent_load
+    return unpickler.load()
+
+def numpy_load_legacy(path):
     deserialized_objects = {}
     def persistent_load(saved_id):
         data = saved_id[1:]
-        data_type, root_key, location, size, view_metadata = data
+        data_type, root_key, location, size, metadata = data
+        obj = data_type(size)
         if root_key not in deserialized_objects:
-            obj = data_type(size)
             deserialized_objects[root_key] = obj
         storage = deserialized_objects[root_key]
         return storage
@@ -81,6 +127,12 @@ def torch_load(path):
 
 def load_torch_checkpoint(path):
     if mode == 'numpy':
-        return numpy_load(path)
+        f = open(path, 'rb')
+        if _is_zipfile(f):
+            f.close()
+            return numpy_load(path)
+        else:
+            f.close()
+            return numpy_load_legacy(path)
     else:
         return torch_load(path)
